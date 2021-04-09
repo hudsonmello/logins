8a6ddb5b416e4088e5d997fdb0d493bdb4b6789c
Hudson <hudson@onixseven.com.br>
2021-04-09 12:55:27 -0300

    ajuste na session_destroy()

--- login_success.php
+++ login_success.php
@@ -5,7 +5,7 @@ session_start();
 if(isset($_SESSION["email"])){
     echo "<h3>Login efetuado com sucesso, seja bem-vindo ". $_SESSION["email"] . "</h3>";
 
-    echo '<br><br><a href="index.php">Sair</a>';
+    echo '<br><br><a href="logout.php">Sair</a>';
 } else {
     header("Location: index.php");
 }
